21f647c65e7a8a7ec42198474bfb78dab8f1264e
김봉상 <tph02361@gmail.com>
2024-07-17 16:55:27 +0900

    [FE] feat: 라우팅, OAuth2 테스트 #S11P11D202-264
    - 임시 Navbar 구현
    - Zustand state 테스트중
    - Google OAuth2 연결 테스트

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthStore } from '../stores/authStore';
+import './NavBar.css';
+
+const NavBar: React.FC = () => {
+  const { isLoggedIn, logout } = useAuthStore();
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login'); // 로그인페이지로 이동
+  };
+
+  const handleLogoutClick = () => {
+    logout(); // 로그아웃
+  };
+
+  return (
+    <nav>
+      <ul>
+        {!isLoggedIn && (
+          <>
+            <li>
+              <Link to="/">로고(홈)</Link>
+            </li>
+            <li>
+              <Link to="/teatime">티타임</Link>
+            </li>
+            <li>
+              <Link to="/sharing">나눔</Link>
+            </li>
+            <li>
+              <button onClick={handleLoginClick}>로그인</button>
+            </li>
+          </>
+        )}
+        {isLoggedIn && (
+          <>
+            <li>
+              <Link to="/">로고(홈)</Link>
+            </li>
+            <li>
+              <Link to="/teatime">티타임</Link>
+            </li>
+            <li>
+              <Link to="/sharing">나눔</Link>
+            </li>
+            <li>
+              <button >마이페이지(구현안되어있음)</button>
+            </li>
+            <li>
+              <button onClick={handleLogoutClick}>로그아웃</button>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ frontend/src/pages/Sharing.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Sharing {}
+
+const Sharing: React.FC = () => {
+  return (
+    <div>
+      <h1>Sharing</h1>
+    </div>
+  );
+};
+
+export default Sharing;
