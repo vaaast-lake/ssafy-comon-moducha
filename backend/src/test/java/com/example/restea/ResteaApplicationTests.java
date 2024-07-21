e9daacb861c6fdd9e9909b275eca574e5ad9d492
tired_i <ramen4598@naver.com>
2024-07-21 21:27:30 +0900

    [BE] chore: Backend 프로젝트 초기 환경설정

---
+++ backend/src/test/java/com/example/restea/ResteaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResteaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
