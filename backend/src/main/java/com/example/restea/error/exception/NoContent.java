7cec6f5ac99e683ecf6e82a66945a5703cdab52b
김의근 <fkaus4598@naver.com>
2024-07-24 17:32:33 +0900

    [BE] feat: RestControllerAdvice를 통한 예외 일괄 처리 #S11P12D202-617

---
+++ backend/src/main/java/com/example/restea/error/exception/NoContent.java
@@ -0,0 +1,18 @@
+package com.example.restea.error.exception;
+
+import java.nio.charset.Charset;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.Nullable;
+import org.springframework.web.client.HttpStatusCodeException;
+
+public final class NoContent extends HttpStatusCodeException {
+  private NoContent(String statusText, HttpHeaders headers, byte[] body, @Nullable Charset charset) {
+    super(HttpStatus.NO_CONTENT, statusText, headers, body, charset);
+  }
+
+  private NoContent(String message, String statusText, HttpHeaders headers, byte[] body, @Nullable Charset charset) {
+    super(message, HttpStatus.NO_CONTENT, statusText, headers, body, charset);
+  }
+
+}
