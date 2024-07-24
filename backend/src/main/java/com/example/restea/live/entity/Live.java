ec9637f2ad104b6e74931d5d6af52c9866559bbe
김소연 <xhwkrlxhdi@naver.com>
2024-07-24 16:51:19 +0900

    [BE] feat: Live Entity 개발 #S11P12D202-509

---
+++ backend/src/main/java/com/example/restea/live/entity/Live.java
@@ -0,0 +1,29 @@
+package com.example.restea.live.entity;
+
+import com.example.restea.common.entity.BaseTimeEntity;
+import com.example.restea.teatime.entity.TeatimeBoard;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
+public class Live extends BaseTimeEntity {
+  @Id
+  @Column(name = "live_id")
+  private String liveId = UUID.randomUUID().toString();
+
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "teatime_board_id")
+  private TeatimeBoard teatimeBoard;
+}
