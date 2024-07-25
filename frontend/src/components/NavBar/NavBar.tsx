3d5cc4994b8cadb2806776984acfd70bb1afe274
최진오 <sidamo.dev@gmail.com>
2024-07-25 14:15:05 +0900

    [FE] feat: NavBar 컴포넌트 레이아웃, 스타일 적용 #S11P12D202-621

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../../stores/authStore';
+import navModucha from '/logo/nav-moducha.svg';
 
 const NavBar = () => {
   const { isLoggedIn, logout } = useAuthStore();
@@ -16,63 +17,101 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-gray-800 text-white h-12 p-4">
-      <ul className="flex justify-around items-center h-full">
-        {!isLoggedIn && (
-          <>
-            <li className="mx-4">
-              <Link to="/" className="font-bold hover:text-gray-300">로고(홈)</Link>
-            </li>
-            <li className="mx-4">
-              <Link to="/teatime" className="font-bold hover:text-gray-300">티타임</Link>
-            </li>
-            <li className="mx-4">
-              <Link to="/sharing" className="font-bold hover:text-gray-300">나눔</Link>
-            </li>
-            <li className="mx-4">
-              <Link to="/webrtc" className="font-bold hover:text-gray-300">WebRTC</Link>
-            </li>
-            <li className="mx-4">
-              <button
-                onClick={handleLoginClick}
-                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300"
-              >
-                로그인
-              </button>
-            </li>
-          </>
-        )}
-        {isLoggedIn && (
-          <>
-            <li className="mx-4">
-              <Link to="/" className="font-bold hover:text-gray-300">로고(홈)</Link>
-            </li>
-            <li className="mx-4">
-              <Link to="/teatime" className="font-bold hover:text-gray-300">티타임</Link>
-            </li>
-            <li className="mx-4">
-              <Link to="/sharing" className="font-bold hover:text-gray-300">나눔</Link>
-            </li>
-            <li className="mx-4">
-              <Link to="/webrtc" className="font-bold hover:text-gray-300">WebRTC</Link>
-            </li>
-            <li className="mx-4">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
-                마이페이지(구현안되어있음)
-              </button>
-            </li>
-            <li className="mx-4">
-              <button
-                onClick={handleLogoutClick}
-                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300"
-              >
-                로그아웃
-              </button>
-            </li>
-          </>
-        )}
-      </ul>
-    </nav>
+    <div className="flex justify-center border-b-2 border-[#eee]">
+      {/* border-bottom: 2px solid #EEE; */}
+      <nav className="flex justify-between w-full lg:w-2/3 h-20 items-center">
+        <Link to="/" className="btn btn-ghost">
+          <img src={navModucha} alt="" />
+        </Link>
+        <ul className="flex shrink-0 justify-between items-center gap-4 mr-5">
+          {!isLoggedIn && (
+            <>
+              <li>
+                <Link
+                  to="/teatime"
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  티타임
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/sharing"
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  나눔
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/webrtc"
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  WebRTC
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={handleLoginClick}
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  로그인
+                </button>
+              </li>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <li>
+                <Link
+                  to="/"
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  로고(홈)
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/teatime"
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  티타임
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/sharing"
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  나눔
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/webrtc"
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  WebRTC
+                </Link>
+              </li>
+              <li>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
+                  마이페이지(구현안되어있음)
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={handleLogoutClick}
+                  className="text-center text-[#6d6d6d] text-lg font-medium hover:text-black"
+                >
+                  로그아웃
+                </button>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
--- frontend/src/pages/Share/ShareDetail.tsx
+++ frontend/src/pages/Share/ShareDetail.tsx
@@ -1,20 +1,34 @@
 import { useEffect, useState } from 'react';
 import { fetchShareDetail } from '../../api/fetchShare';
 import { useParams } from 'react-router-dom';
-import { ShareListItem } from '../../types/ShareType';
+import { ShareDetailItem } from '../../types/ShareType';
+import ArticleCard from '../../components/Article/ArticleCard';
+import { ShareDetailResponse } from '../../constants/shareResponseTest';
 
 const ShareDetail = () => {
-  const [shareDetail, setsShareDetail] = useState<ShareListItem>();
+  const [shareDetail, setsShareDetail] = useState<ShareDetailItem>(
+    ShareDetailResponse.data
+  );
   const { shareId } = useParams();
   useEffect(() => {
+    // 임의 라우팅 -> shareId가 undefined인 경우 guard 추가
     fetchShareDetail(shareId)
-      .then((res) => setsShareDetail(res.data))
+      .then((res) => setsShareDetail(res.data.data))
       .catch((err) => console.log(err));
-  });
+  }, [shareId]);
   return (
-    <>
-      <div></div>
-    </>
+    <div className="grid grid-cols-12 h-screen">
+      <aside className="hidden lg:flex col-span-2 bg-gray-300">leftAside</aside>
+      <main className="col-span-12 lg:col-span-8 grid grid-cols-12">
+        <aside className="col-span-4 p-2">
+          <ArticleCard {...shareDetail} />
+        </aside>
+        <main className="col-span-8 bg-success p-2">innerArticle</main>
+      </main>
+      <aside className="hidden lg:flex col-span-2 bg-gray-300">
+        reightAside
+      </aside>
+    </div>
   );
 };
 
