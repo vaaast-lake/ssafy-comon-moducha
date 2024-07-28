6a08463c567989e2c5e5199e7beabbf6db0fbf07
김의근 <fkaus4598@naver.com>
2024-07-28 21:53:07 +0900

    [BE] fix: GlobalExceptionHandler IllegalStateException 문제 해결 #S11P12D202-655

--- backend/src/main/java/com/example/restea/error/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/restea/error/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.restea.error;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -25,13 +26,19 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(ResponseStatusException.class)
   public ResponseEntity<String> handleResponseStatusException(ResponseStatusException e) {
-    log.error("Response Status Error : ", e);
+    log.error("Response Status Error : ", e.getMessage());
     return ResponseEntity.status(e.getStatusCode()).build();
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    log.error("Method Argument Not Valid Error : ", e.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleException(Exception e) {
-    log.error("서버 에러 발생 : ", e);
+    log.error("서버 에러 발생 : ", e.getMessage());
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
   }
 }
