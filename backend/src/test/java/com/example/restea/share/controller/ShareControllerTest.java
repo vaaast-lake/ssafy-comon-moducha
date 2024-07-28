648f4491a63207b1ae1039db76f0c6adcb96e9cd
김의근 <fkaus4598@naver.com>
2024-07-28 17:39:08 +0900

    [BE] test: 나눔게시판 글 조회 기능 테스트 코드 작성 #S11P12D202-607

--- backend/src/test/java/com/example/restea/share/controller/ShareControllerTest.java
+++ backend/src/test/java/com/example/restea/share/controller/ShareControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.restea.share.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.restea.oauth2.dto.CustomOAuth2User;
@@ -9,12 +11,14 @@ import com.example.restea.oauth2.service.CustomOAuth2UserService;
 import com.example.restea.share.dto.ShareCreationRequest;
 import com.example.restea.share.entity.ShareBoard;
 import com.example.restea.share.repository.ShareBoardRepository;
+import com.example.restea.user.entity.User;
 import com.example.restea.user.repository.UserRepository;
 import com.example.restea.util.SecurityTestUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -252,4 +256,94 @@ class ShareControllerTest {
     assertThat(shareBoards.size()).isEqualTo(0);
   }
 
+  @DisplayName("getShareBoard : 나눔 게시판 조회 성공.")
+  @Test
+  public void getShareBoard_Success() throws Exception {
+    // given
+    CustomOAuth2User oAuth2User = custumOAuth2UserService.handleNewUser("authId2", "authToken2");
+    Optional<User> userOptional = userRepository.findByAuthId("authId2");
+    if (userOptional.isEmpty()) {
+      throw new RuntimeException("테스트를 위한 유저 생성 실패");
+    }
+    User user = userOptional.get();
+
+    ShareBoard createdShareBoard = shareBoardRepository.save(ShareBoard.builder()
+        .title("TestTitle")
+        .content("TestContent")
+        .maxParticipants(10)
+        .endDate(LocalDateTime.now().plusWeeks(1L))
+        .user(user)
+        .build());
+
+    final String url = "/api/v1/shares/" + createdShareBoard.getId();
+
+    // when
+    ResultActions result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON_VALUE));
+
+    // then
+    result.andExpect(status().isOk())
+        .andExpect(jsonPath("$.data.shareBoardId").value(createdShareBoard.getId()))
+        .andExpect(jsonPath("$.data.title").value(createdShareBoard.getTitle()))
+        .andExpect(jsonPath("$.data.content").value(createdShareBoard.getContent()))
+        .andExpect(jsonPath("$.data.maxParticipants").value(createdShareBoard.getMaxParticipants()))
+//        저장하면서 형식이 약간 수정해서 완벽하게 일치하기 어렵다.
+//        .andExpect(jsonPath("$.data.endDate").value(createdShareBoard.getEndDate().toString().substring(0, 26)))
+        .andExpect(jsonPath("$.data.viewCount").value(1))
+        .andExpect(jsonPath("$.data.participants").value(0))
+        .andExpect(jsonPath("$.data.nickname").value(user.getExposedNickname()));
+  }
+
+  @DisplayName("getShareBoard : 나눔 게시판 조회 : 비활성화된 유저.")
+  @Transactional
+  @Test
+  public void getShareBoard_deactivatedUser() throws Exception {
+    // given
+    CustomOAuth2User oAuth2User = custumOAuth2UserService.handleNewUser("authId2", "authToken2");
+    Optional<User> userOptional = userRepository.findByAuthId("authId2");
+    if (userOptional.isEmpty()) {
+      throw new RuntimeException("테스트를 위한 유저 생성 실패");
+    }
+    User user = userOptional.get();
+
+    ShareBoard createdShareBoard = shareBoardRepository.save(ShareBoard.builder()
+        .title("TestTitle")
+        .content("TestContent")
+        .maxParticipants(10)
+        .endDate(LocalDateTime.now().plusWeeks(1L))
+        .user(user)
+        .build());
+
+    // 유저 비활성화
+    user.deactivate();
+
+    final String url = "/api/v1/shares/" + createdShareBoard.getId();
+
+    // when
+    ResultActions result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON_VALUE));
+
+    // then
+    result.andExpect(status().isOk())
+        .andExpect(jsonPath("$.data.shareBoardId").value(createdShareBoard.getId()))
+        .andExpect(jsonPath("$.data.title").value(createdShareBoard.getTitle()))
+        .andExpect(jsonPath("$.data.content").value(createdShareBoard.getContent()))
+        .andExpect(jsonPath("$.data.maxParticipants").value(createdShareBoard.getMaxParticipants()))
+//        저장하면서 형식이 약간 수정해서 완벽하게 일치하기 어렵다.
+//        .andExpect(jsonPath("$.data.endDate").value(createdShareBoard.getEndDate().toString().substring(0, 26)))
+        .andExpect(jsonPath("$.data.viewCount").value(1))
+        .andExpect(jsonPath("$.data.participants").value(0))
+        .andExpect(jsonPath("$.data.nickname").value("탈퇴한 유저"));
+  }
+
+  @DisplayName("getShareBoard : 존재하지 않는 나눔 게시판 조회로 인한 실패.")
+  @Test
+  public void getShareBoard_fail() throws Exception {
+    // given
+    final String url = "/api/v1/shares/999";
+
+    // when
+    ResultActions result = mockMvc.perform(get(url).accept(MediaType.APPLICATION_JSON_VALUE));
+
+    // then
+    result.andExpect(status().isNotFound());
+  }
 }
