bfc1bf1cf17b2ae4d0858edd31a4ed41a4598520
김의근 <fkaus4598@naver.com>
2024-07-31 00:09:25 +0900

    [BE] chore: ShareBoardRepository 메서드 삭제 #S11P12D202-567

--- backend/src/main/java/com/example/restea/share/repository/ShareBoardRepository.java
+++ backend/src/main/java/com/example/restea/share/repository/ShareBoardRepository.java
@@ -1,15 +1,12 @@
 package com.example.restea.share.repository;
 
 import com.example.restea.share.entity.ShareBoard;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ShareBoardRepository extends JpaRepository<ShareBoard, Integer> {
-
-  List<ShareBoard> findAllByActivated(boolean b);
 
   Long countByActivated(boolean b);
 
