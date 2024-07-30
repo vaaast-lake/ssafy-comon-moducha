de94c45cab6d6c0c6078f5192f82a75126bee242
김소연 <xhwkrlxhdi@naver.com>
2024-07-30 10:16:09 +0900

    [BE] fix: 양뱡항에서 단방향으로 바꾸면서 생긴 오류로 TeatimeBoard에서 Live참고하는 함수 삭제 #S11P12D202-744

--- backend/src/main/java/com/example/restea/teatime/repository/TeatimeBoardRepository.java
+++ backend/src/main/java/com/example/restea/teatime/repository/TeatimeBoardRepository.java
@@ -1,12 +1,9 @@
 package com.example.restea.teatime.repository;
 
-import com.example.restea.live.entity.Live;
 import com.example.restea.teatime.entity.TeatimeBoard;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeatimeBoardRepository extends JpaRepository<TeatimeBoard, Integer> {
-  Optional<TeatimeBoard> findByLive(Live live);
 }
