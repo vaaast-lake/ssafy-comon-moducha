1a3b81bc3596663cdb56c00dc68d46377efb4e98
권대호 <kown9512@gmail.com>
2024-07-30 15:48:49 +0900

    [FE] refactor: WebRTC configureUrls() 위치 변경 #S11P12D202-534

--- frontend/src/pages/WebRTC/WebRTC.tsx
+++ frontend/src/pages/WebRTC/WebRTC.tsx
@@ -11,36 +11,8 @@ import RoomChatting from './components/RoomChatting';
 import RoomSharingButton from './components/RoomSharingButton';
 import { Message, TrackInfo } from '../../types/WebRTCType';
 
-// For local development, leave these variables empty
-// For production, configure them with correct URLs depending on your deployment
-const { APPLICATION_SERVER_URL, LIVEKIT_URL } = configureUrls();
-
-function configureUrls() {
-  let APPLICATION_SERVER_URL = '';
-  let LIVEKIT_URL = '';
-  // If APPLICATION_SERVER_URL is not configured, use default value from local development
-  if (!APPLICATION_SERVER_URL) {
-    if (window.location.hostname === 'localhost') {
-      APPLICATION_SERVER_URL = 'http://localhost:6080/';
-    } else {
-      APPLICATION_SERVER_URL = 'https://' + window.location.hostname + ':6443/';
-    }
-  }
-
-  // If LIVEKIT_URL is not configured, use default value from local development
-  if (!LIVEKIT_URL) {
-    if (window.location.hostname === 'localhost') {
-      LIVEKIT_URL = 'ws://localhost:7880/';
-    } else {
-      LIVEKIT_URL = 'wss://' + window.location.hostname + ':7443/';
-    }
-  }
-  return { APPLICATION_SERVER_URL, LIVEKIT_URL };
-}
-
-
-
 export default function WebRTC() {
+  const { APPLICATION_SERVER_URL, LIVEKIT_URL } = configureUrls();
   const [room, setRoom] = useState<Room | undefined>(undefined);
   const [roomName, setRoomName] = useState('Test Room');
   const [participantName, setParticipantName] = useState(
@@ -66,6 +38,8 @@ export default function WebRTC() {
     setRoom(undefined);
     setLocalTrack(undefined);
     setRemoteTracks([]);
+    setMessages([]);
+    setIsScreenSharing(false);
   }
 
   return (
@@ -86,33 +60,56 @@ export default function WebRTC() {
           leaveRoom={leaveRoom}
         />
       ) : (
-        <>
-          <div id="room">
-            <RoomHeader 
-              roomName={roomName}
-              leaveRoom={leaveRoom}
+        <div id="room">
+          <RoomHeader 
+            roomName={roomName}
+            leaveRoom={leaveRoom}
+          />
+          <RoomVideoAudioTracks
+            localTrack={localTrack}
+            remoteTracks={remoteTracks}
+            participantName={participantName}
+          />
+          <RoomChatting 
+            room={room}
+            messages={messages}
+            setMessages={setMessages}
+            inputMessage={inputMessage}
+            setInputMessage={setInputMessage}
+          />
+          {/* 화면 공유 버튼 */}
+          <RoomSharingButton 
+            room={room}
+            isScreenSharing={isScreenSharing}
             />
-            <RoomVideoAudioTracks
-              localTrack={localTrack}
-              remoteTracks={remoteTracks}
-              participantName={participantName}
-            />
-            <RoomChatting 
-              room={room}
-              messages={messages}
-              setMessages={setMessages}
-              inputMessage={inputMessage}
-              setInputMessage={setInputMessage}
-            />
-            {/* 화면 공유 버튼 */}
-            <RoomSharingButton 
-              room={room}
-              isScreenSharing={isScreenSharing}
-              />
-            {/* 화면 공유 버튼 끗 */}
-          </div>
-        </>
+          {/* 화면 공유 버튼 끗 */}
+        </div>
       )}
     </>
   );
+}
+
+// For local development, leave these variables empty
+// For production, configure them with correct URLs depending on your deployment
+function configureUrls() {
+  let APPLICATION_SERVER_URL = '';
+  let LIVEKIT_URL = '';
+  // If APPLICATION_SERVER_URL is not configured, use default value from local development
+  if (!APPLICATION_SERVER_URL) {
+    if (window.location.hostname === 'localhost') {
+      APPLICATION_SERVER_URL = 'http://localhost:6080/';
+    } else {
+      APPLICATION_SERVER_URL = 'https://' + window.location.hostname + ':6443/';
+    }
+  }
+
+  // If LIVEKIT_URL is not configured, use default value from local development
+  if (!LIVEKIT_URL) {
+    if (window.location.hostname === 'localhost') {
+      LIVEKIT_URL = 'ws://localhost:7880/';
+    } else {
+      LIVEKIT_URL = 'wss://' + window.location.hostname + ':7443/';
+    }
+  }
+  return { APPLICATION_SERVER_URL, LIVEKIT_URL };
 }
