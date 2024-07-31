00d83000a8143284c24d543a92644ce696298fd6
최진오 <sidamo.dev@gmail.com>
2024-07-31 23:39:17 +0900

    [FE] chore: 불필요한 fragment 제거

--- frontend/src/pages/Share/components/ShareCard.tsx
+++ frontend/src/pages/Share/components/ShareCard.tsx
@@ -7,38 +7,36 @@ const ShareCard = ({
   title,
   maxParticipants,
   participants,
-  viewCount
+  viewCount,
 }: ShareListItem) => {
   return (
-    <>
-      <Link
-        to={`${boardId}`}
-        className="flex bg-base-100 overflow-hidden shadow rounded-lg transition ease-in-out hover:bg-beige hover:text-wood hover:shadow-lg duration-150"
-      >
-        <figure className="size-32 shrink-0">
-          <img
-            className=""
-            src="https://the-chinese-tea-company.com/cdn/shop/files/Loose_Leaf_Tea_1600x.jpg?v=1613558529"
-            alt="Album"
-          />
-        </figure>
-        <article className="flex flex-col w-full p-2 h-32 overflow-hidden">
-          <h2 className="font-bold truncate">{title}</h2>
-          <section className="flex items-center gap-3">
-            <div>
-              <UserIcon className="inline size-4" />
-              <span className="text-sm">
-                {participants} / {maxParticipants}
-              </span>
-            </div>
-            <div>
-              <EyeIcon className="inline size-4" />
-              <span className="text-sm ms-1">{viewCount}</span>
-            </div>
-          </section>
-        </article>
-      </Link>
-    </>
+    <Link
+      to={`${boardId}`}
+      className="flex bg-base-100 overflow-hidden shadow rounded-lg transition ease-in-out hover:bg-beige hover:text-wood hover:shadow-lg duration-150"
+    >
+      <figure className="size-32 shrink-0">
+        <img
+          className=""
+          src="https://the-chinese-tea-company.com/cdn/shop/files/Loose_Leaf_Tea_1600x.jpg?v=1613558529"
+          alt="Album"
+        />
+      </figure>
+      <article className="flex flex-col w-full p-2 h-32 overflow-hidden">
+        <h2 className="font-bold truncate">{title}</h2>
+        <section className="flex items-center gap-3">
+          <div>
+            <UserIcon className="inline size-4" />
+            <span className="text-sm">
+              {participants} / {maxParticipants}
+            </span>
+          </div>
+          <div>
+            <EyeIcon className="inline size-4" />
+            <span className="text-sm ms-1">{viewCount}</span>
+          </div>
+        </section>
+      </article>
+    </Link>
   );
 };
 
