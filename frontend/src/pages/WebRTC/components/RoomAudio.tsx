a9995ea307f12d02056f44d9519705677e3c403a
권대호 <kown9512@gmail.com>
2024-08-01 13:18:55 +0900

    Revert "[FE] feat: 원격 사용자 음소거 기능 구현 #S11P12D202-755"

--- frontend/src/pages/WebRTC/components/RoomAudio.tsx
+++ frontend/src/pages/WebRTC/components/RoomAudio.tsx
@@ -1,12 +1,16 @@
 import { LocalAudioTrack, RemoteAudioTrack } from 'livekit-client';
 import { useEffect, useRef } from 'react';
+import useConfigureUrls from '../../../hooks/useConfigureUrls';
+import axios from 'axios';
 
-interface AudioComponentProps {
+interface RoomAudioProps {
   track: LocalAudioTrack | RemoteAudioTrack;
+  participantIdentity: string;
 }
 
-export default function RoomAudio({ track }: AudioComponentProps) {
+export default function RoomAudio({ track, participantIdentity }: RoomAudioProps) {
   const audioElement = useRef<HTMLAudioElement | null>(null);
+  const { APPLICATION_SERVER_URL } = useConfigureUrls();
 
   useEffect(() => {
     if (audioElement.current) {
@@ -18,5 +22,37 @@ export default function RoomAudio({ track }: AudioComponentProps) {
     };
   }, [track]);
 
-  return <audio ref={audioElement} id={track.sid} />;
+  const handleMuteUser = () => {
+    axios({
+      method: 'post',
+      url: `${APPLICATION_SERVER_URL}/teatimes/7/lives/mute/1`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        userId: `${participantIdentity}`,
+        trackSid: `${track.sid}`
+      }
+    })
+    .then((res) => {
+      console.log(res);
+      
+    })
+    .catch((err) => {
+      console.log(err);
+      
+    })
+  }
+
+  return (
+    <div id='audio-box'>
+      <audio ref={audioElement} id={track.sid} />
+      <button
+        className="bg-yellow-500 text-white px-4 py-1 rounded ml-2 hover:bg-yellow-600"
+        onClick={handleMuteUser}
+      >
+        REMOTE USER MUTE AUDIO
+      </button>
+    </div> 
+  );
 }
