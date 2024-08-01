7e7b647e7af8e65ed131ea4e1433332f06beb70b
김봉상 <tph02361@gmail.com>
2024-08-01 14:11:35 +0900

    [FE] refactor: Navbar.tsx에서 logout api 분리 #S11P12D202

---
+++ frontend/src/api/logout.ts
@@ -0,0 +1,38 @@
+// src/api/logout.ts
+export const logout = async (): Promise<void> => {
+  const accessToken = localStorage.getItem('authorization');
+  if (!accessToken) {
+    throw new Error('No access token found');
+  }
+  // const response = await fetch(
+  //   `${import.meta.env.VITE_API_URL}/api/v1/logout`,
+  //   {
+  //     method: 'POST',
+  //     headers: {
+  //       Authorization: `Bearer ${accessToken}`,
+  //       'Content-Type': 'application/json',
+  //     },
+  //   }
+  // );
+  // if (!response.ok) {
+  //   console.log('Logout failed');
+  //   throw new Error('Logout failed');
+  // }
+  localStorage.removeItem('authorization');
+};
+
+// 로그아웃을 처리하는 함수
+export const handleLogout = async (
+  setLoggedIn: (loggedIn: boolean) => void,
+  setCurrentUsername: (username: string) => void,
+  navigate: (path: string) => void
+) => {
+  try {
+    await logout(); // 로그아웃 API 호출
+    setLoggedIn(false); // zustand에서 로그인 상태를 false로 설정
+    setCurrentUsername(''); // zustand에서 닉네임을 ''으로 설정
+    navigate('/'); // 홈으로 리디렉션
+  } catch (error) {
+    console.error('Logout error:', error);
+  }
+};
