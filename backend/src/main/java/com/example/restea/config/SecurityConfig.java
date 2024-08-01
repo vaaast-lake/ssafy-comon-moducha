e273d51b3be5b457f3912d4763dd49752439335e
김범중 <rlaqjawnd625@naver.com>
2024-08-01 10:50:15 +0900

    [BE] refactor: CORS 허용 URL을 Nginx로 변경 및 환경변수로 설정 #S11P12D202-764

--- backend/src/main/java/com/example/restea/config/SecurityConfig.java
+++ backend/src/main/java/com/example/restea/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import com.example.restea.oauth2.service.CustomOAuth2UserService;
 import com.example.restea.user.repository.UserRepository;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,6 +31,9 @@ public class SecurityConfig {
     private final RefreshTokenRepository refreshTokenRepository;
     private final UserRepository userRepository;
 
+    @Value("${cors.url}")
+    private String corsURL;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -40,7 +44,7 @@ public class SecurityConfig {
                     CorsConfiguration configuration = new CorsConfiguration();
 
                     configuration.setAllowedOrigins(
-                            Collections.singletonList("http://localhost:5173"));
+                            Collections.singletonList(corsURL));
                     configuration.setAllowedMethods(Collections.singletonList("*"));
                     configuration.setAllowCredentials(true);
                     configuration.setAllowedHeaders(Collections.singletonList("*"));
