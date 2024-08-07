f70e41ccb3f689c345ed82b6cb418e336522b00f
김봉상 <tph02361@gmail.com>
2024-08-07 14:27:01 +0900

    [FE] chore: authStore에서 닉네임 저장방법 변경(username -> nickname) #S11P12D202-864

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { jwtDecode } from 'jwt-decode';
+// 백엔드에서 accesstoken의 key인 username을 nickname으로 변경하여 코드를 수정하였습니다.
+// 프론트엔드에서 currentUsername으로 쓰는 변수는 그대로 유지하였습니다. 따라서 authStore를 참조하는 코드들은 변경할 필요가 없습니다.
 interface AuthState {
   isLoggedIn: boolean;
   setLoggedIn: (status: boolean) => void;
@@ -15,7 +17,7 @@ interface AuthState {
 }
 
 interface TokenType {
-  username: string;
+  nickname: string;
   userId: string;
   role: string;
 }
@@ -24,7 +26,7 @@ const useAuthStore = create<AuthState>()(
   devtools((set) => {
     const token = localStorage.getItem('authorization');
     const isLoggedIn = token ? true : false;
-    const currentUsername = token ? jwtDecode<TokenType>(token).username : '';
+    const currentUsername = token ? jwtDecode<TokenType>(token).nickname : '';
     const currentUserId = token ? jwtDecode<TokenType>(token).userId : '';
     const currentUserRole = token ? jwtDecode<TokenType>(token).role : '';
     return {
