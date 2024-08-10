f81bd6d481dff7c1113322837e7f13b2b45cc6ba
김소연 <xhwkrlxhdi@naver.com>
2024-08-10 22:11:33 +0900

    [BE] feat: 티타임 취소 기능 구현 #S11P12D202-927

--- backend/src/main/java/com/example/restea/teatime/service/TeatimeParticipantService.java
+++ backend/src/main/java/com/example/restea/teatime/service/TeatimeParticipantService.java
@@ -5,9 +5,11 @@ import static com.example.restea.teatime.enums.TeatimeBoardMessage.TEATIME_BOARD
 import static com.example.restea.teatime.enums.TeatimeParticipantMessage.TEATIME_PARTICIPANT_AFTER_END_DATE;
 import static com.example.restea.teatime.enums.TeatimeParticipantMessage.TEATIME_PARTICIPANT_ALREADY_EXISTS;
 import static com.example.restea.teatime.enums.TeatimeParticipantMessage.TEATIME_PARTICIPANT_FULL;
+import static com.example.restea.teatime.enums.TeatimeParticipantMessage.TEATIME_PARTICIPANT_NOT_FOUND;
 import static com.example.restea.teatime.util.TeatimeUtil.getActivatedTeatimeBoard;
 import static com.example.restea.teatime.util.TeatimeUtil.getActivatedUser;
 
+import com.example.restea.teatime.dto.TeatimeCancelResponse;
 import com.example.restea.teatime.dto.TeatimeJoinRequest;
 import com.example.restea.teatime.dto.TeatimeJoinResponse;
 import com.example.restea.teatime.entity.TeatimeBoard;
@@ -16,6 +18,7 @@ import com.example.restea.teatime.repository.TeatimeBoardRepository;
 import com.example.restea.teatime.repository.TeatimeParticipantRepository;
 import com.example.restea.user.entity.User;
 import com.example.restea.user.repository.UserRepository;
+import com.example.restea.user.service.UserService;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -31,6 +34,7 @@ public class TeatimeParticipantService {
     private final TeatimeParticipantRepository teatimeParticipantRepository;
     private final TeatimeBoardRepository teatimeBoardRepository;
     private final UserRepository userRepository;
+    private final UserService userService;
 
     @Transactional
     public TeatimeJoinResponse addParticipant(Integer teatimeBoardId, TeatimeJoinRequest request, Integer userId) {
@@ -56,6 +60,28 @@ public class TeatimeParticipantService {
         teatimeParticipantRepository.save(teatimeParticipant);
 
         return TeatimeJoinResponse.of(teatimeParticipant);
+    }
+
+    @Transactional
+    public TeatimeCancelResponse cancelParticipation(Integer teatimeBoardId, Integer userId,
+                                                     Integer customOAuth2UserId) {
+        User activatedUser = userService.checkValidUser(customOAuth2UserId, userId);
+        TeatimeBoard activatedTeatimeBoard = getActivatedTeatimeBoard(teatimeBoardRepository, teatimeBoardId);
+
+        if (Objects.equals(activatedTeatimeBoard.getUser().getId(), activatedUser.getId())) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, TEATIME_BOARD_WRITER.getMessage());
+        }
+
+        validateEndDate(activatedTeatimeBoard.getEndDate());
+
+        TeatimeParticipant participant = teatimeParticipantRepository.findByTeatimeBoardAndUser(activatedTeatimeBoard,
+                        activatedUser)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        TEATIME_PARTICIPANT_NOT_FOUND.getMessage()));
+
+        teatimeParticipantRepository.delete(participant);
+
+        return TeatimeCancelResponse.of(teatimeBoardId, userId);
     }
 
     private boolean checkActivatedTeatimeBoardWriter(TeatimeBoard activatedTeatimeBoard, User activatedUser) {
