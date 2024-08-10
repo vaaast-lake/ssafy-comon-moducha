af5b9283b710d6ac6f86a31a6eaa811ef6fe30db
김소연 <xhwkrlxhdi@naver.com>
2024-08-10 17:17:04 +0900

    [BE] feat: 티타임 참가 신청 기능 구현 #S11P12D202-926

--- backend/src/main/java/com/example/restea/teatime/enums/TeatimeParticipantMessage.java
+++ backend/src/main/java/com/example/restea/teatime/enums/TeatimeParticipantMessage.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TeatimeParticipantMessage {
-    TEATIME_PARTICIPANT_NOT_FOUND("Not a participant.");
+    TEATIME_PARTICIPANT_NOT_FOUND("Not a participant."),
+    TEATIME_PARTICIPANT_ALREADY_EXISTS("Already exists."),
+    TEATIME_PARTICIPANT_FULL("참가 인원이 초과되었습니다.");
 
     private final String message;
 }
