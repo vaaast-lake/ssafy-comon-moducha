cb8bacdf442614fe008bcfdf43f3d55763b248da
김범중 <rlaqjawnd625@naver.com>
2024-08-11 14:24:19 +0900

    [BE] feat: 글 수정 시 본문에 이용되지 않는 이미지를 S3 버킷에서 제거하는 기능 개발 #S11P12D202-923
    - 추후 ImageRequest를 구현하는 클래스를 제네릭으로 이용하기 위함

--- backend/src/main/java/com/example/restea/s3/service/S3Service.java
+++ backend/src/main/java/com/example/restea/s3/service/S3Service.java
@@ -1,9 +1,14 @@
 package com.example.restea.s3.service;
 
+import com.example.restea.common.dto.ImageRequest;
 import java.io.IOException;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface S3Service {
 
     String uploadSingleFile(MultipartFile multipartFile) throws IOException;
+
+    <T extends ImageRequest> void deleteImagesNotUsedInContent(T request);
+
+    <T extends ImageRequest> void deleteImagesNotUsedInContent(T request, String contentBefore, String contentAfter);
 }
