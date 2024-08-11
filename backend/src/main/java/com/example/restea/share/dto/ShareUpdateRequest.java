38bb99c9a20911300dd5e4333c66e07e39635842
김범중 <rlaqjawnd625@naver.com>
2024-08-11 14:22:56 +0900

    [BE] refactor: 글 등록, 수정 RequestDto가 ImageRequest 인터페이스를 구현하도록 변경 #S11P12D202-923
    - 추후 ImageRequest를 구현하는 클래스를 제네릭으로 이용하기 위함

--- backend/src/main/java/com/example/restea/share/dto/ShareUpdateRequest.java
+++ backend/src/main/java/com/example/restea/share/dto/ShareUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.example.restea.share.dto;
 
+import com.example.restea.common.dto.ImageRequest;
 import com.example.restea.share.entity.ShareBoard;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Max;
@@ -8,6 +9,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @Getter
-public class ShareUpdateRequest {
+public class ShareUpdateRequest implements ImageRequest {
 
     @NotBlank(message = "empty title.")
     @Size(max = 50, message = "title is too long.")
@@ -33,6 +35,8 @@ public class ShareUpdateRequest {
     @Min(value = 1, message = "maxParticipants must be greater than 0.")
     @Max(value = 100, message = "maxParticipants must be less than 100.")
     private Integer maxParticipants;
+
+    private List<String> images;
 
     public ShareBoard toEntity() {
         return ShareBoard.builder()
