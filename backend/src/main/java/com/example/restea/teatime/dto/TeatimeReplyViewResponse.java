22fd50cb0bd361552b626cd52d47d766b1f515f0
김소연 <xhwkrlxhdi@naver.com>
2024-08-12 02:05:14 +0900

    [BE] feat: 티타임 게시판 대댓글 조회 ResponseDTO 생성 #S11P12D202-947

---
+++ backend/src/main/java/com/example/restea/teatime/dto/TeatimeReplyViewResponse.java
@@ -0,0 +1,28 @@
+package com.example.restea.teatime.dto;
+
+import com.example.restea.teatime.entity.TeatimeReply;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TeatimeReplyViewResponse {
+    private final Integer commentId;
+    private final Integer replyId;
+    private final String content;
+    private final LocalDateTime createdDate;
+    private final Integer userId;
+    private final String nickname;
+
+    public static TeatimeReplyViewResponse of(TeatimeReply teatimeReply) {
+        return TeatimeReplyViewResponse.builder()
+                .commentId(teatimeReply.getTeatimeComment().getId())
+                .replyId(teatimeReply.getId())
+                .content(teatimeReply.getExposedContent())
+                .createdDate(teatimeReply.getCreatedDate())
+                .userId(teatimeReply.getUser().getId())
+                .nickname(teatimeReply.getExposedNickname())
+                .build();
+    }
+}
