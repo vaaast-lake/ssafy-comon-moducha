744adf3e274db47be0969fbc07b813c93b82c1b3
김소연 <xhwkrlxhdi@naver.com>
2024-08-12 02:49:55 +0900

    [BE] feat: 티타임 게시판 대댓글 생성 기능 구현 #S11P12D202-947

--- backend/src/main/java/com/example/restea/teatime/controller/TeatimeReplyController.java
+++ backend/src/main/java/com/example/restea/teatime/controller/TeatimeReplyController.java
@@ -1,16 +1,23 @@
 package com.example.restea.teatime.controller;
 
 import com.example.restea.common.dto.ResponseDTO;
+import com.example.restea.oauth2.dto.CustomOAuth2User;
+import com.example.restea.teatime.dto.TeatimeReplyCreationRequest;
+import com.example.restea.teatime.dto.TeatimeReplyCreationResponse;
 import com.example.restea.teatime.dto.TeatimeReplyViewResponse;
 import com.example.restea.teatime.service.TeatimeReplyService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +54,28 @@ public class TeatimeReplyController {
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(result);
+    }
+
+    /**
+     * 주어진 티타임 게시글 및 댓글에 답글 작성
+     *
+     * @param teatimeBoardId   티타임 게시글 ID.
+     * @param teatimeCommentId 티타임 댓글 ID.
+     * @param request          content 댓글 내용.
+     * @param customOAuth2User 현재 인증된 사용자.
+     * @return 작성한 답글 정보를 포함하는 ResponseEntity 객체를 반환합니다. 답글 작성에 실패하면 에러 코드를 담은 ResponseEntity를 반환합니다.
+     */
+    @PostMapping("/replies")
+    public ResponseEntity<ResponseDTO<?>> createTeatimeReply(
+            @PathVariable("teatimeBoardId") Integer teatimeBoardId,
+            @PathVariable("teatimeCommentId") Integer teatimeCommentId,
+            @Valid @RequestBody TeatimeReplyCreationRequest request,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+
+        TeatimeReplyCreationResponse result = teatimeReplyService.createTeatimeReply(teatimeBoardId, teatimeCommentId,
+                request.getContent(), customOAuth2User.getUserId());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ResponseDTO.from(result));
     }
 }
--- backend/src/main/java/com/example/restea/teatime/service/TeatimeReplyService.java
+++ backend/src/main/java/com/example/restea/teatime/service/TeatimeReplyService.java
@@ -1,16 +1,23 @@
 package com.example.restea.teatime.service;
 
+import static com.example.restea.share.util.ShareUtil.getActivatedUser;
+import static com.example.restea.teatime.enums.TeatimeBoardMessage.TEATIME_BOARD_USER_NOT_ACTIVATED;
 import static com.example.restea.teatime.enums.TeatimeCommentMessage.TEATIME_COMMENT_NOT_FOUND;
 import static com.example.restea.teatime.util.TeatimeUtil.getActivatedTeatimeBoard;
+import static com.example.restea.teatime.util.TeatimeUtil.getActivatedTeatimeComment;
 
 import com.example.restea.common.dto.PaginationDTO;
 import com.example.restea.common.dto.ResponseDTO;
+import com.example.restea.teatime.dto.TeatimeReplyCreationResponse;
 import com.example.restea.teatime.dto.TeatimeReplyViewResponse;
 import com.example.restea.teatime.entity.TeatimeBoard;
 import com.example.restea.teatime.entity.TeatimeComment;
 import com.example.restea.teatime.entity.TeatimeReply;
 import com.example.restea.teatime.repository.TeatimeBoardRepository;
 import com.example.restea.teatime.repository.TeatimeReplyRepository;
+import com.example.restea.user.entity.User;
+import com.example.restea.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -25,6 +32,7 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class TeatimeReplyService {
 
+    private final UserRepository userRepository;
     private final TeatimeReplyRepository teatimeReplyRepository;
     private final TeatimeBoardRepository teatimeBoardRepository;
 
@@ -40,6 +48,27 @@ public class TeatimeReplyService {
         PaginationDTO pagination = PaginationDTO.of(teatimeReplies.getTotalPages(), page, perPage);
 
         return ResponseDTO.of(data, pagination);
+    }
+
+    @Transactional
+    public TeatimeReplyCreationResponse createTeatimeReply(Integer teatimeBoardId, Integer teatimeCommentId,
+                                                           String content, Integer userId) {
+
+        User activatedUser = getActivatedUser(userRepository, userId);
+        TeatimeBoard activatedTeatimeBoard = getActivatedTeatimeBoard(teatimeBoardRepository, teatimeBoardId);
+        checkActivatedTeatimeBoardWriter(activatedTeatimeBoard);
+
+        TeatimeComment activatedTeatimeComment = getActivatedTeatimeComment(teatimeCommentId, activatedTeatimeBoard);
+
+        TeatimeReply teatimeReply = TeatimeReply.builder()
+                .content(content)
+                .teatimeComment(activatedTeatimeComment)
+                .user(activatedUser)
+                .build();
+
+        teatimeReplyRepository.save(teatimeReply);
+
+        return TeatimeReplyCreationResponse.of(teatimeReply, activatedUser.getExposedNickname());
     }
 
     public TeatimeComment getTeatimeComment(Integer teatimeCommentId, TeatimeBoard activatedTeatimeBoard) {
@@ -60,5 +89,12 @@ public class TeatimeReplyService {
             data.add(TeatimeReplyViewResponse.of(teatimeReply));
         }
         return data;
+    }
+
+    private static void checkActivatedTeatimeBoardWriter(TeatimeBoard activatedTeatimeBoard) {
+        if (activatedTeatimeBoard.getUser().getActivated()) {
+            return;
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, TEATIME_BOARD_USER_NOT_ACTIVATED.getMessage());
     }
 }
