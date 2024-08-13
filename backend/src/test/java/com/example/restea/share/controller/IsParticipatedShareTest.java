ce795803ccbbaf4a531375596cb8b48b086bc483
김의근 <fkaus4598@naver.com>
2024-08-13 09:59:00 +0900

    [BE] test: 나눔 목록 조회 및 검색, 나눔 참가 여부 확인 테스트 코드 추가 #S11P12D202-999

--- backend/src/test/java/com/example/restea/share/controller/IsParticipatedShareTest.java
+++ backend/src/test/java/com/example/restea/share/controller/IsParticipatedShareTest.java
@@ -1,13 +1,20 @@
 package com.example.restea.share.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.example.restea.ResteaApplication;
 import com.example.restea.oauth2.dto.CustomOAuth2User;
 import com.example.restea.oauth2.service.CustomOAuth2UserService;
+import com.example.restea.share.entity.ShareBoard;
 import com.example.restea.share.repository.ShareBoardRepository;
+import com.example.restea.user.entity.User;
 import com.example.restea.user.repository.UserRepository;
 import com.example.restea.util.SecurityTestUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,8 +22,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -66,15 +75,69 @@ class IsParticipatedShareTest {
         userRepository.deleteAll();
     }
 
-    @DisplayName("")
+    @DisplayName("나눔 참가 여부 조회 성공 - 참가한 나눔 (글쓴이)")
     @Test
-    void test1() throws Exception {
+    public void isParticipate_writer_success() throws Exception {
+        // given
+        User writer = userRepository.findByAuthIdAndActivated("authId", true)
+                .orElseThrow(() -> new RuntimeException("테스트를 위한 유저 생성 실패"));
+        ShareBoard shareBoard = createShareBoard(writer, 3);
+
+        final String url = "/api/v1/shares/" + shareBoard.getId() + "/participants/" + writer.getId();
+
+        // when
+        ResultActions result = mockMvc.perform(get(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.data.boardId").value(shareBoard.getId()));
+        result.andExpect(jsonPath("$.data.userId").value(writer.getId()));
+        result.andExpect(jsonPath("$.data.participated").value(true));
     }
 
-    // 나눔 참가 여부 조회 성공 - 참가한 나눔 (글쓴이)
+    @DisplayName("나눔 참가 여부 조회 성공 - 참가한 나눔 (글쓴이)")
+    @Test
+    public void isParticipate_user_success() throws Exception {
+        // given
+        custumOAuth2UserService.handleNewUser("authId2", "authToken2");
+        User writer = userRepository.findByAuthIdAndActivated("authId2", true)
+                .orElseThrow(() -> new RuntimeException("테스트를 위한 유저 생성 실패"));
+        ShareBoard shareBoard = createShareBoard(writer, 3);
+
+        User user = userRepository.findByAuthIdAndActivated("authId", true)
+                .orElseThrow(() -> new RuntimeException("테스트를 위한 유저 생성 실패"));
+
+        final String url = "/api/v1/shares/" + shareBoard.getId() + "/participants/" + user.getId();
+
+        // when
+        ResultActions result = mockMvc.perform(get(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.data.boardId").value(shareBoard.getId()));
+        result.andExpect(jsonPath("$.data.userId").value(user.getId()));
+        result.andExpect(jsonPath("$.data.participated").value(false));
+    }
     // 나눔 참가 여부 조회 성공 - 참가한 나눔 (참가자)
     // 나눔 참가 여부 조회 성공 - 참가하지 않은 나눔
     // 나눔 참가 여부 조회 실패 - 유효하지 않는 사용자
     // 나눔 참가 여부 조회 실패 - 유효하지 않는 나눔
     // 나눔 참가 여부 조회 실패 - 다른 사람에 대한 조회
+
+    private ShareBoard createShareBoard(User writer, Integer maxParticipants) {
+        final String title = "Title";
+        final String shareBoardContent = "Content";
+        final LocalDateTime endDate = LocalDateTime.now().plusWeeks(1L);
+        return shareBoardRepository.save(ShareBoard.builder()
+                .title(title)
+                .content(shareBoardContent)
+                .maxParticipants(maxParticipants)
+                .endDate(endDate)
+                .user(writer)
+                .build());
+    }
 }
