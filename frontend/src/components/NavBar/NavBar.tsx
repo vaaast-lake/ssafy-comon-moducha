c09c83f44db8a567ef8aa55a2a13a141bd2651f9
최진오 <sidamo.dev@gmail.com>
2024-08-15 15:14:32 +0900

    [FE] feat: 이미지 용량 초과 toast 알림, NavBar 로고 반응형 적용 #S11P12D202-1053

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import useAuthStore from '../../stores/authStore';
 import { handleLogout } from '../../api/logout'; // handleLogout 함수 import
 import navModucha from '/logo/nav-moducha.svg'; // navModucha 이미지 경로
-
+import navModuchaMini from '/logo/nav-moducha-mini.svg';
 const NavBar = () => {
   const navigate = useNavigate();
   const { isLoggedIn, setLoggedIn, setCurrentUsername } = useAuthStore(
@@ -26,9 +26,15 @@ const NavBar = () => {
       <nav className="flex justify-between w-full lg:w-3/5 h-16 items-center">
         <Link
           to="/"
-          className="btn btn-ghost hover:bg-none rounded-xl hover:bg-inherit"
+          className="btn btn-ghost hover:bg-none rounded-xl hover:bg-inherit hidden sm:flex"
         >
-          <img src={navModucha} alt="Nav Logo" />
+          <img src={navModucha} alt="nav-logo" />
+        </Link>
+        <Link
+          to="/"
+          className="btn btn-ghost hover:bg-none rounded-xl hover:bg-inherit sm:hidden"
+        >
+          <img src={navModuchaMini} alt="nav-logo_mini" />
         </Link>
         <ul className="flex shrink-0 justify-between items-center gap-4 mr-5">
           {!isLoggedIn && (
